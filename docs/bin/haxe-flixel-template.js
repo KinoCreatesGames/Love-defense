29abfabbcd05a33fc4e33a31d203fd8391a24f06
kinoar <kinarose@gmail.com>
2021-02-20 17:38:42 +0000

    Update game website

--- docs/bin/haxe-flixel-template.js
+++ docs/bin/haxe-flixel-template.js
@@ -869,7 +869,7 @@ ApplicationMain.main = function() {
 ApplicationMain.create = function(config) {
 	var app = new openfl_display_Application();
 	ManifestResources.init(config);
-	app.meta.h["build"] = "38";
+	app.meta.h["build"] = "39";
 	app.meta.h["company"] = "KinoCreatesGames";
 	app.meta.h["file"] = "haxe-flixel-template";
 	app.meta.h["name"] = "Love Defense";
@@ -67792,7 +67792,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 632825;
+	this.version = 541290;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = "lime.utils.AssetCache";
